e0cb521ee968a604b119948f4264155cf5ed24db
Dexter Lakin <dexterlakin@hotmail.com>
2021-04-04 19:07:30 +0000

    2.1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -45,6 +45,7 @@ const App = () => {
       </div>
     </div>
   )
+
 }
 
 export default App
---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => <h1>{course.name}</h1>
+
+const Total = ({ course }) => <p>Number of exercises {course.parts.reduce((a, b) => a.exercises + b.exercises, 0)}</p>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ course }) => <div>{course.parts.map(part => <Part part={part} key={part.id}/>)}</div>
+
+const Course = ({ course }) => {
+  return(
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return(
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
