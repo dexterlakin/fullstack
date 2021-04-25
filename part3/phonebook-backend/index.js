9f48f9b4f619be364d7299b3a13c170db942bbda
Dexter Lakin <dexter.lakin@vonage.com>
2021-04-25 17:55:41 +0100

    3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,6 +3,10 @@ const app = express()
 
 app.use(express.json())
 
+const cors = require('cors')
+
+app.use(cors())
+
 let persons = [
   {
     "name": "Arto Hellas",
@@ -99,7 +103,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
