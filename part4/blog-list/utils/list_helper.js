81a49d40a9f1256ca559f9a249bc6005a30af3be
Dexter Lakin <dexter.lakin@vonage.com>
2021-05-16 14:03:01 +0100

    4.4

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,7 +1,12 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => blogs.reduce((total, blog) => total + blog.likes, 0)
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
