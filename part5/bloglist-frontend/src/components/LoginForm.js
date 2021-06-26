b2d5c2a845bcf823d65b1e56a2b68df3246c79d6
Dexter Lakin <dexter.lakin@vonage.com>
2021-06-26 10:44:04 +0100

    5.11, 5.12

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
-   handleSubmit,
-   handleUsernameChange,
-   handlePasswordChange,
-   username,
-   password
-  }) => {
+  handleSubmit,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password
+}) => {
   return (
     <div>
       <h2>Login</h2>
@@ -15,6 +16,7 @@ const LoginForm = ({
         <div>
           username
           <input
+            id='username'
             value={username}
             onChange={handleUsernameChange}
           />
@@ -22,15 +24,24 @@ const LoginForm = ({
         <div>
           password
           <input
+            id='password'
             type="password"
             value={password}
             onChange={handlePasswordChange}
           />
-      </div>
-        <button type="submit">login</button>
+        </div>
+        <button id="login-button" type="submit">login</button>
       </form>
     </div>
   )
+}
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
 }
 
 export default LoginForm
