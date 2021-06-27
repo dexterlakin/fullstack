c7f44d521062a05446fa21616dacc5d9577c6606
Dexter Lakin <dexterlakin@hotmail.com>
2021-06-27 12:37:29 +0100

    5.17

---
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -0,0 +1,16 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Dexter Lakin',
+      username: 'dlakin',
+      password: 'dlakin'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user) 
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('log in').click()
+  })
+})
