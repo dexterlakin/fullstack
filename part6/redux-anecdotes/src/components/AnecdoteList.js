25315815b4c739e6fdd49a3c36ee256de0187967
Dexter Lakin <dexter.lakin@vonage.com>
2021-08-02 14:12:30 +0100

    6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -14,7 +14,6 @@ const AnecdoteList = () => {
 
   return (
     <div>
-      <h2>Anecdotes</h2>
       {[...anecdotes].sort((a, b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
